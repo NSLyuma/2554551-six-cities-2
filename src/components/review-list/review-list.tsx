60785f398b276101b74d8d10987ec0ec001d72c6
Anastasiya <arsaidaz10@mts.ru>
2025-01-10 19:40:38 +0300

    Remove comment

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -1,18 +1,14 @@
-/* eslint-disable arrow-body-style */
-
 import { SingleComment } from '../../lib/types/comment';
 import ReviewItem from '../review-item/review-item';
 
 type Props = { reviews: SingleComment[] };
 
-const ReviewList = ({ reviews }: Props) => {
-  return (
-    <ul className="reviews__list">
-      {reviews.map((review) => (
-        <ReviewItem key={review.id} review={review} />
-      ))}
-    </ul>
-  );
-};
+const ReviewList = ({ reviews }: Props) => (
+  <ul className="reviews__list">
+    {reviews.map((review) => (
+      <ReviewItem key={review.id} review={review} />
+    ))}
+  </ul>
+);
 
 export default ReviewList;
